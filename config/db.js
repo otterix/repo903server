ba134925e7a7c7c51a1d2101b93b395597463227
Edmond Tang <otterix@gmail.com>
2017-03-10 23:51:09 +1100

    added db config

---
+++ config/db.js
@@ -0,0 +1,19 @@
+var config = {
+  development: {
+    //url to be used in link generation
+    url: 'mongodb://903admin:903admin@ds119750.mlab.com:19750/903takings_mongodb'
+    //mongodb connection settings
+    //database: { host: '127.0.0.1', port: '27016', db: 'site' },
+    //server details
+    //server: { host: '127.0.0.1', port: '3420' }
+  },
+  production: {
+    //url to be used in link generation
+    url: 'mongodb://903admin:903admin@ds119750.mlab.com:19750/903takings_mongodb'
+    //mongodb connection settings
+    //database: { host: '127.0.0.1', port: '27017', db: 'site' },
+    //server details
+    //server: { host: '127.0.0.1', port: '3421' }
+  }
+};
+module.exports = config;
