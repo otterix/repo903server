fe4b92be4e42f1d863414cb69b3cb0a48bc8ae35
Edmond Tang <otterix@gmail.com>
2017-03-10 23:47:31 +1100

    updated server.js

--- server.js
+++ server.js
@@ -2,9 +2,12 @@
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
-const db             = require('./config/db');
+//const db             = require('./config/db');
 
 const app            = express();
+
+const envnode = process.env.NODE_ENV || 'development';
+const db = require('./config/db')[envnode];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', (process.env.PORT || 8000));
